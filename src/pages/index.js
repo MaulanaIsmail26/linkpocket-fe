94ce2d0536b092cdf4997c08257a5e3a6bbf1295
Maulana Ismail <maulanaismail3466@gmail.com>
2023-06-16 21:36:21 +0700

    [ADD] create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import style from "@/styles/pages/home.module.scss";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,231 +14,68 @@ export default function Home() {
       </Head>
       <main className={style.main}>
         <section className={`container-fluid ${style.homePage}`}>
-          <div className={`container`}>
-            <div className={`row`}>
-              <div className={`col-12 position-relative ${style.cardHolder}`}>
-                {/* ICON AND BUTTON LOGIN OR REGISTER */}
-                {/* <div
-                  className={`position-absolute top-0 start-50 translate-middle-x p-0 ${style.iconBtnAuth}`}
-                >
-                  TITLE CARD
-                  <div className={`row ${style.titleCard}`}>
-                    <div className={`col`}>
-                      <div className="d-flex justify-content-between">
-                        <Image
-                          src={require("/public/images/Icon-app-nooutline.png")}
-                          className={` ${style.iconApp}`}
-                          // width={500}
-                          height={65}
-                          alt="Icon-Linkpocket"
-                        />
-                        <div className={` ${style.btnLoginRegister}`}>
-                          <button
-                            type="button"
-                            className={`btn btn-outline-primary ${style.btnLogin}`}
-                          >
-                            Login
-                          </button>
-                          <button
-                            type="button"
-                            className={`btn btn-primary ${style.btnRegister}`}
-                          >
-                            Register
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
-                {/* CARD FORM REGISTER */}
-                <div
-                  className={`position-absolute top-50 start-50 translate-middle px-4 ${style.cardLinkPocket}`}
-                >
-                  <div className={`row ${style.titleCard}`}>
-                    <div className={`col`}>
-                      <div className="d-flex justify-content-between">
-                        <Image
-                          src={require("/public/images/Icon-app-nooutline.png")}
-                          className={` ${style.iconApp}`}
-                          // width={500}
-                          height={65}
-                          alt="Icon-Linkpocket"
-                        />
-                        <div className={` ${style.btnLoginRegister}`}>
-                          <button
-                            type="button"
-                            className={`btn btn-outline-primary ${style.btnLogin}`}
-                          >
-                            Login
-                          </button>
-                          <button
-                            type="button"
-                            className={`btn btn-primary ${style.btnRegister}`}
-                          >
-                            Register
-                          </button>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  {/* PROFILE SECTION */}
-                  <div className={`row ${style.profileCard}`}>
-                    <div className="col-4">
+          <div className={`row`}>
+            <div className={`col-12 position-relative ${style.colum}`}>
+              <div
+                className={`position-absolute top-50 start-50 translate-middle ${style.blurOverlay}`}
+              >
+                <div className={``}>
+                  {/* ICON APP */}
+                  <div className={`row mb-2 ${style.iconApp}`}>
+                    <div className="col d-flex justify-content-center">
                       <Image
-                        src={require("/public/images/IMG_20230116_093528.jpg")}
-                        className={` ${style.photoProfile}`}
+                        src={require("/public/images/Icon-app-nooutline.png")}
+                        className={` ${style.icon}`}
                         // width={500}
-                        // height={65}
+                        height={80}
                         alt="Icon-Linkpocket"
                       />
                     </div>
-                    <div className="col-8">
-                      <h3 className={`${style.username}`}>Maulana Ismail</h3>
-                      <p className={`${style.desc}`}>
-                        Terjemahkan teks & berkas dokumen secara instan.
-                        Terjemahan.
-                      </p>
-                      <div className="d-grid gap-2">
-                        <button
-                          type="button"
-                          className={`btn btn-primary ${style.btnShare}`}
-                        >
-                          Share your Linkpocket card
-                        </button>
-                      </div>
+                  </div>
+                  {/* TEXT WELCOME */}
+                  <div className={`row ${style.textWelcome}`}>
+                    <div className="col d-flex justify-content-center">
+                      <h1 className={`${style.text}`}>Welcome To LinkPocket</h1>
                     </div>
                   </div>
-
-                  {/* LINKS SECTION */}
-                  <div className={`row ${style.linkSection}`}>
-                    <div className="col-12">
-                      <div className="d-grid gap-2 ">
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Facebook
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          YouTube
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Instagram
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Twitter
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Tiktok
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          GitHub
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          E-mail
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          WhatsApp
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Shopee
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          LinkedIn
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          WhatsApp
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Shopee
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          LinkedIn
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          WhatsApp
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Shopee
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          LinkedIn
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          WhatsApp
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          Shopee
-                        </button>
-                        <button
-                          className={`btn ${style.linkStick}`}
-                          type="button"
-                        >
-                          LinkedIn
-                        </button>
-                        <style>
-                          {`
-                            ::-webkit-scrollbar {
-                              width: 0.3em;
-                              height: 0.5em;
-                            }
-                            ::-webkit-scrollbar-thumb {
-                              background-color: rgba(0, 0, 0, 0.2);
-                            }
-                          `}
-                        </style>
-                      </div>
+                  {/* TEXT SLOGAN*/}
+                  <div className={`row ${style.textSlogan}`}>
+                    <div className="col d-flex justify-content-center">
+                      <p className={`${style.text}`}>
+                        Collect all your links in just one pocket
+                      </p>
+                    </div>
+                  </div>
+                  {/* BIG ICON*/}
+                  <div className={`row mb-1 ${style.bigIcon}`}>
+                    <div className="col d-flex justify-content-center">
+                      <Image
+                        src={require("/public/images/iconApp.png")}
+                        className={` ${style.icon}`}
+                        // width={500}
+                        height={250}
+                        alt="Icon-Linkpocket"
+                      />
+                    </div>
+                  </div>
+                  {/* TEXT STARTED */}
+                  <div className={`row ${style.TextStarted}`}>
+                    <div className="col d-flex justify-content-center">
+                      <p className={`${style.started}`}>
+                        Its time for you to make a link pocket
+                      </p>
+                    </div>
+                  </div>
+                  {/* BUTTON STARTED */}
+                  <div className={`row ${style.btnStarted}`}>
+                    <div className="col d-flex justify-content-center">
+                      <Link
+                        type="button"
+                        className={`btn btn-primary ${style.btn}`}
+                        href={"/auth/register"}
+                      >
+                        Get Started
+                      </Link>
                     </div>
                   </div>
                 </div>
