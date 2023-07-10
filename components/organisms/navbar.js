3d33dadee0dbb0b49ff83c5f42cd67e76ac36285
Maulana Ismail <maulanaismail3466@gmail.com>
2023-07-10 21:42:12 +0700

    [ADD] Integration to be

--- components/organisms/navbar.js
+++ components/organisms/navbar.js
@@ -1,36 +1,73 @@
-import React from 'react'
-import Image from 'next/image';
-import style from "@/styles/pages/profile.module.scss";
-import Link from 'next/link';
+import React from "react";
+import Image from "next/image";
+import styleNavbar from "@/styles/navbar.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+// ICON
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export default function Navbar() {
+  const router = useRouter();
+  const [isLogin, setIsLogin] = React.useState(false);
+
+  const token = localStorage.getItem("token");
+
+  // check if already login
+  React.useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      setIsLogin(true);
+    } else {
+      setIsLogin(false);
+    }
+  }, []);
+
+  const checkProfile = localStorage.getItem("profile")
+    ? JSON.parse(localStorage.getItem("profile"))
+    : null;
+  const [profile, setProfile] = React.useState(checkProfile);
+
   return (
     <div>
       <div className={`col`}>
         <div className="d-flex justify-content-between">
           <Image
             src={require("/public/images/Icon-app-nooutline.webp")}
-            className={` ${style.iconApp}`}
+            className={` ${styleNavbar.iconApp}`}
             // width={500}
             // height={65}
             alt="Icon-Linkpocket"
           />
-          <div className={` ${style.btnLoginRegister}`}>
-            <Link
-              type="button"
-              className={`btn btn-outline-primary ${style.btnLogin}`}
-              href={"/auth/login"}
-            >
-              Login
-            </Link>
-            <Link
-              type="button"
-              className={`btn btn-primary ${style.btnRegister}`}
-              href={"/auth/register"}
-            >
-              Register
-            </Link>
-          </div>
+          {isLogin ? (
+            <>
+              <Link href={`/profile/${profile.fullname}`}>
+                <AccountCircleIcon
+                  style={{ fontSize: "35px", color: "#03e9f4" }}
+                />
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className={` ${styleNavbar.btnLoginRegister}`}>
+                <Link
+                  type="button"
+                  className={`btn btn-outline-primary ${styleNavbar.btnLogin}`}
+                  href={"/auth/login"}
+                >
+                  Login
+                </Link>
+                <Link
+                  type="button"
+                  className={`btn btn-primary ${styleNavbar.btnRegister}`}
+                  href={"/auth/register"}
+                >
+                  Register
+                </Link>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
